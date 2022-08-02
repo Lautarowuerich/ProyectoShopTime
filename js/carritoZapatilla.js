f0cacb46cefae8efe690154825a4c6bc7a7ed33b
Lautarowuerich <millo.capo127@gmail.com>
2022-08-01 23:22:08 -0300

    cuarto commit

--- js/carritoZapatilla.js
+++ js/carritoZapatilla.js
@@ -1,3 +1,4 @@
+//---- VARIABLES
 const contenedorProductos = document.getElementById('contenedor');
 const carritoContenedor = document.getElementById('carrito-contenedor');
 const botonVaciar = document.getElementById('vaciarCarrito');
@@ -6,6 +7,7 @@ const precioTotal = document.getElementById('precioTotal');
 
 let carrito = [];
 
+//----AL VOLVER A CARGAR LA PÁGINA QUE SE MANTENGAN LOS ITEMS DEL CARRITO
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('carrito')){
         carrito = JSON.parse(localStorage.getItem('carrito'))
@@ -13,12 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
-
+//---- BOTON PARA VACIAR EL CARRITO
 botonVaciar.addEventListener('click',() => {
     carrito.length = 0;
     actualizarCarrito();
 })
 
+
+//---- RENDERIZAR PRODUCTOS EN HTML
 stockProductos4.forEach((producto) => {
     const div = document.createElement ('div');
     div.classList.add('productos');
@@ -31,18 +35,21 @@ stockProductos4.forEach((producto) => {
 
     contenedorProductos.appendChild(div);
 
+    //---- AGREGA CADA PRODUCTO CON SU RESPECTIVO ID DENTRO DEL CARRITO
     const boton = document.getElementById(`agregar${producto.id}`);
-
     boton.addEventListener('click', () => {
         agregarCarrito(producto.id);
     })
 
 })
 
+//---- AGREGAR EL PRODUCTO AL CARRITO
 const agregarCarrito = (prodId) => {
 
+    //---- NO DUPLICA PRODUCTOS EN EL CARRITO
     const noDuplicar = carrito.some (prod => prod.id === prodId);
 
+    //---- SI YA EXISTE EL PRODUCTO EN EL CARRITO NO LO REPITE Y AGREGA CANTIDAD
     if (noDuplicar){
         const prod = carrito.map(prod =>{
             if(prod.id === prodId){
@@ -50,6 +57,7 @@ const agregarCarrito = (prodId) => {
             }
         })
     }
+    //---- SI EL PRODUCTO NO ESTÁ REPETIDO SE AGREGA AL CARRITO
     else{
     
         const item = stockProductos4.find((prod) => prod.id === prodId);
@@ -58,6 +66,7 @@ const agregarCarrito = (prodId) => {
         console.log(carrito);
     }
 
+    //---- LIBRERA TOASTY
     Toastify({
         text: "Se agregó producto al carrito",
         duration: 1000,
@@ -69,6 +78,7 @@ const agregarCarrito = (prodId) => {
     actualizarCarrito();
 }
 
+//---- ELIMINA EL PRODUCTO DEL CARRITO
 const eliminarDelCarrito = (productoId) => {
     const item = carrito.find((producto) => producto.id === productoId);
     const indice = carrito.indexOf(item);
@@ -81,6 +91,7 @@ const actualizarCarrito = () => {
 
     carritoContenedor.innerHTML = ""
 
+    //---- CREA UN DIV POR CADA ELEMENTO AGREGADO
     carrito.forEach((producto) =>{
         const div = document.createElement('div');
         div.className = ('productoEnCarrito')
@@ -92,13 +103,16 @@ const actualizarCarrito = () => {
         <button onclick="eliminarDelCarrito(${producto.id})" class="boton-eliminar"><i class= "fas fa-trash-alt">
         `
 
+        //---- A LOS ELEMENTOS AGREGADOS AL CARRITO LE HACEMOS UN APPENCHILD AL MODAL (CONTENEDOR DEL CARRITO)
         carritoContenedor.appendChild(div);
 
         localStorage.setItem('carrito', JSON.stringify(carrito))
     })
 
+    //---- SUMA LA CANTIDAD DE PRODUCTOS AL NUMERO DEL CARRITO
     contadorCarrito.innerText = carrito.length;
 
+    //---- ARRANCANDO DE 0, POR CADA PRODUCTO AGREGADO SUMA LA PROPIEDAD PRECIO PARA EL PRECIO FINAL
     precioTotal.innerText = carrito.reduce((acc,prod) => acc + prod.precio, 0);
 
 
